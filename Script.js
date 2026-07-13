28818214d8cf35caf2c156785dde50b79f37001e
aijanarated <janahani017@gmail.com>
2026-07-14 00:41:40 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,25 @@
+const loadingScreen = document.getElementById("loading-screen");
+const mainMenu = document.getElementById("main-menu");
+const enterBtn = document.getElementById("enter-btn");
 
+// Hide the menu while loading
+mainMenu.style.display = "none";
+
+// After 5 seconds...
+setTimeout(() => {
+    loadingScreen.style.opacity = "0";
+    loadingScreen.style.transition = "1s";
+
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+        mainMenu.style.display = "flex";
+    }, 1000);
+
+}, 5000);
+
+// Enter button
+enterBtn.addEventListener("click", () => {
+
+    alert("Welcome to Mazen's Birthday World! 🎉");
+
+});
